763b9f6eada79317d48c8ab44e275f88cf2be6b5
pcwl <freedompcwl@qq.com>
2019-08-02 14:16:49 +0800

    update hardware api

--- nodemcu/serialcom/serialcom.ino
+++ nodemcu/serialcom/serialcom.ino
@@ -1,8 +1,12 @@
+const int DIRPin = D6; //define Direction pin
+const int PULPin = D5; //define Pulse pin
 String comdata = "";
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    pinMode (PULPin, OUTPUT);
+    pinMode (DIRPin, OUTPUT);
 }
 
 void loop()
@@ -14,9 +18,28 @@ void loop()
     }
     if (comdata.length() > 0)
     {
-        if(!comdata.compareTo("‘{“Action”:”OpenDoor”,”Number”:”01”}’")){
-            Serial.print("da\"ta");
+        if(!comdata.compareTo("{\"Action\": \"OpenDoor\", \"Number\": \"01\"}")){
+            digitalWrite(DIRPin, LOW);
+            for (int i = 0; i < 6400 * 2; i++) {
+              digitalWrite(PULPin, HIGH);
+              delayMicroseconds(50);
+              digitalWrite(PULPin, LOW);
+              delayMicroseconds(50);
+              yield();
+              }
+            Serial.print("{\"Action\": \"OpenDoor\", \"Number\": \"01\", \"info\": 1}");
             }
+        else if(!comdata.compareTo("{\"Action\": \"CloseDoor\", \"Number\": \"01\"}")){
+          digitalWrite(DIRPin, HIGH);
+          for (int i = 0; i < 6400 * 2; i++) {
+            digitalWrite(PULPin, HIGH);
+            delayMicroseconds(50);
+            digitalWrite(PULPin, LOW);
+            delayMicroseconds(50);
+            yield();
+            }
+          Serial.print("{\"Action\": \"CloseDoor\", \"Number\": \"01\", \"info\": 1}");
+          }
         else{Serial.print("unknow");}
         comdata = "";
     }
